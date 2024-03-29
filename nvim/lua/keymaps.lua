09e33005dc4c24cd10b2b9955c310ea80aaea2e5
Jacobo Perez <Jacobo.Perez@charter.com>
2024-03-29 16:11:22 -0600

    Remove duplicate key binding for navigating in neovim

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -3,34 +3,28 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set('n', '<leader>h', '<Cmd>noh<CR>', { desc = 'Clear [H]ighlight' })
-vim.keymap.set('n', '<leader>q', '<Cmd>q<CR>', { desc = '[Q]uit' })
-vim.keymap.set('n', '<leader>w', '<Cmd>w<CR>', { desc = '[W]rite' })
-vim.keymap.set('n', '<leader>c', '<Cmd>bdelete<CR>', { desc = '[C]lose Current Buffer' })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<leader>h", "<Cmd>noh<CR>", { desc = "Clear [H]ighlight" })
+vim.keymap.set("n", "<leader>q", "<Cmd>q<CR>", { desc = "[Q]uit" })
+vim.keymap.set("n", "<leader>w", "<Cmd>w<CR>", { desc = "[W]rite" })
+vim.keymap.set("n", "<leader>c", "<Cmd>bdelete<CR>", { desc = "[C]lose Current Buffer" })
 
 -- Esc in insert mode
-vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set("i", "jk", "<Esc>")
 
 -- Navigation
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-
--- Better window movement
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
