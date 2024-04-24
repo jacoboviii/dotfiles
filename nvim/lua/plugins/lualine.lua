aed174b85a222928c779a4eb5a77c2f8cf2b11ef
Jacobo Perez <Jacobo.Perez@charter.com>
2024-04-24 14:35:22 -0600

    Add relative file path to status bar

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,12 +1,20 @@
 return {
-  -- Set lualine as statusline
-  'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'catppuccin',
-      disabled_filetypes = { 'neo-tree' },
-    },
-  },
+	-- Set lualine as statusline
+	"nvim-lualine/lualine.nvim",
+	-- See `:help lualine.txt`
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "catppuccin",
+			disabled_filetypes = { "neo-tree" },
+		},
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					path = 1, -- 1: Relative path
+				},
+			},
+		},
+	},
 }
