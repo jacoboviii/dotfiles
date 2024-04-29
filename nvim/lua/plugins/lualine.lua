c8533f02444d90baec268407ea4927754d5358f5
Jacobo Perez <Jacobo.Perez@charter.com>
2024-04-29 12:19:17 -0600

    Enable macro messages on lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,19 +2,29 @@ return {
 	-- Set lualine as statusline
 	"nvim-lualine/lualine.nvim",
 	-- See `:help lualine.txt`
-	opts = {
-		options = {
-			icons_enabled = true,
-			theme = "catppuccin",
-			disabled_filetypes = { "neo-tree" },
-		},
-		sections = {
-			lualine_c = {
-				{
-					"filename",
-					path = 1, -- 1: Relative path
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "catppuccin",
+				disabled_filetypes = { "neo-tree" },
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1, -- 1: Relative path
+					},
+				},
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+					},
+					"fileformat",
+					"filetype",
 				},
 			},
-		},
-	},
+		})
+	end,
 }
--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -4,17 +4,11 @@ return {
 	opts = {
 		-- add any options here
 		presets = {
-			bottom_search = false,        -- use a classic bottom cmdline for search
+			bottom_search = false, -- use a classic bottom cmdline for search
 			command_palette = false,
 			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = true,            -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false,       -- add a border to hover docs and signature help
-		},
-		routes = {
-			{
-				view = "notify",
-				filter = { event = "msg_showmode" },
-			},
+			inc_rename = true, -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = false, -- add a border to hover docs and signature help
 		},
 	},
 	dependencies = {
